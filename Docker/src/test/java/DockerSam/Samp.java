8320921c7914bbb43ae3febe45623722adfe76d9
Juliot Sophia M <juliotsophia@gmail.com>
2023-10-27 11:54:14 +0530

    Docker

---
+++ Docker/src/test/java/DockerSam/Samp.java
@@ -0,0 +1,20 @@
+package DockerSam;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Samp {
+	
+	
+	public static void main(String[] args) {
+		
+		 System.setProperty("webdriver.chromedriver","E:\\gokila\\chromedriver.exe");
+	     WebDriver driver = new FirefoxDriver();		
+	     driver.get("https://www.google.com/");
+	     	
+		
+		
+		
+	}
+
+}
